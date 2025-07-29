91fb7f95a7946e39835f00d2cbd27fb1d79d36fb
Daniel-Cdemy <daniel_martin@teilnehmer.cdemy.de>
2025-07-29 15:27:11 +0200

    Add first id's. Add function

--- function.js
+++ function.js
@@ -1,1 +1,22 @@
-alert('Javascript funktioniert!');
+const dateToday = new Date();
+    let day = dateToday.getDay();
+        let dayName = getDayName();
+            document.getElementById("weekDay").textContent = dayName;
+            document.getElementById("weekDay1").textContent = dayName;
+
+        function getDayName() {
+            if (day === 0) 
+                return "Sonntag";
+            else if (day === 1) 
+                return "Montag";
+            else if (day === 2) 
+                return "Dienstag";
+            else if (day === 3) 
+                return "Mittwoch";
+            else if (day === 4) 
+                return "Donnerstag";
+            else if (day === 5) 
+                return "Freitag";
+            else if (day === 6) 
+                return "Samstag";
+            }
