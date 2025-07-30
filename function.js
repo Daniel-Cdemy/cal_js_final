c3e8da39f13b946c566837ebbbb976ac37bde8a9
Daniel-Cdemy <daniel_martin@teilnehmer.cdemy.de>
2025-07-30 08:54:34 +0200

    Add function getMonth

--- function.js
+++ function.js
@@ -19,5 +19,39 @@ const dateToday = new Date();
                 return "Samstag";
             }
 
-            document.getElementById("weekDay").textContent = dayName;
-            document.getElementById("weekDay1").textContent = dayName;
+    document.getElementById("weekDay").textContent = dayName;
+    document.getElementById("weekDay1").textContent = dayName;
+
+    let month = dateToday.getMonth();
+        let monthName = getMonthName(month);
+
+        function getMonthName() {
+            if (month === 0)
+                return "Januar";
+            if (month === 1)
+                return "Februar";
+            if (month === 2)
+                return "März";
+            if (month === 3)
+                return "April";
+            if (month === 4)
+                return "Mai";
+            if (month === 5)
+                return "Juni";
+            if (month === 6)
+                return "Juli";
+            if (month === 7)
+                return "August";
+            if (month === 8)
+                return "September";
+            if (month === 9)
+                return "Oktober";
+            if (month === 10)
+                return "November";
+            if (month === 11)
+                return "Dezember";
+        }
+
+    document.getElementById("fullMonth").textContent = monthName
+    document.getElementById("fullMonth1").textContent = monthName
+    document.getElementById("fullMonth2").textContent = monthName
