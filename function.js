f6f1187e1e40f77c75a084c98596bb568ed3ea00
Daniel-Cdemy <daniel_martin@teilnehmer.cdemy.de>
2025-07-31 14:34:26 +0200

    Clean code in js

--- function.js
+++ function.js
@@ -2,36 +2,11 @@ const dateToday = new Date();
 let weekday = dateToday.getDay();
 let dayName = getDayName(weekday);
 
-function getDayName() {
-  if (weekday === 0) return "Sonntag";
-  else if (weekday === 1) return "Montag";
-  else if (weekday === 2) return "Dienstag";
-  else if (weekday === 3) return "Mittwoch";
-  else if (weekday === 4) return "Donnerstag";
-  else if (weekday === 5) return "Freitag";
-  else if (weekday === 6) return "Samstag";
-}
-
 document.getElementById("weekDay").textContent = dayName;
 document.getElementById("weekDay1").textContent = dayName;
 
 let month = dateToday.getMonth();
 let monthName = getMonthName(month);
-
-function getMonthName() {
-  if (month === 0) return "Januar";
-  if (month === 1) return "Februar";
-  if (month === 2) return "März";
-  if (month === 3) return "April";
-  if (month === 4) return "Mai";
-  if (month === 5) return "Juni";
-  if (month === 6) return "Juli";
-  if (month === 7) return "August";
-  if (month === 8) return "September";
-  if (month === 9) return "Oktober";
-  if (month === 10) return "November";
-  if (month === 11) return "Dezember";
-}
 
 document.getElementById("fullMonth").textContent = monthName;
 document.getElementById("fullMonth1").textContent = monthName;
@@ -42,12 +17,7 @@ document.getElementById("fullMonth5").textContent = monthName;
 
 let year = dateToday.getFullYear();
 let isLeapYear = (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;
-let daysInYear = getDaysInYear();
-
-function getDaysInYear() {
-  if (isLeapYear === true) return 366;
-  if (isLeapYear === false) return 365;
-}
+let daysInYear = getDaysInYear(year);
 
 const dateStart = new Date(year, 0, 1);
 let diffInMS = dateToday - dateStart;
@@ -69,14 +39,6 @@ let day = dateToday.getDate();
 let wievielte = getWievielte(day);
 datePeriod = day + ".";
 
-function getWievielte() {
-  if (day < 8) return "erste";
-  if (day < 15) return "zweite";
-  if (day < 22) return "dritte";
-  if (day < 29) return "vierte";
-  if (day > 29) return "fünfte";
-}
-
 const lastDayInMonth = new Date(year, month + 1, 0).getDate();
 
 document.getElementById("lastDayInMonth").textContent = lastDayInMonth;
@@ -89,6 +51,46 @@ document.getElementById("wievielte").textContent = wievielte;
 const newYearsDay = new Date(year, 0, 1);
 const laborDay = new Date(year, 4, 1);
 const germanUnityDay = new Date(year, 9, 3);
+
+document.getElementById("holiday").textContent = isHoliday();
+
+function getDayName() {
+  if (weekday === 0) return "Sonntag";
+  else if (weekday === 1) return "Montag";
+  else if (weekday === 2) return "Dienstag";
+  else if (weekday === 3) return "Mittwoch";
+  else if (weekday === 4) return "Donnerstag";
+  else if (weekday === 5) return "Freitag";
+  else if (weekday === 6) return "Samstag";
+}
+
+function getMonthName() {
+  if (month === 0) return "Januar";
+  if (month === 1) return "Februar";
+  if (month === 2) return "März";
+  if (month === 3) return "April";
+  if (month === 4) return "Mai";
+  if (month === 5) return "Juni";
+  if (month === 6) return "Juli";
+  if (month === 7) return "August";
+  if (month === 8) return "September";
+  if (month === 9) return "Oktober";
+  if (month === 10) return "November";
+  if (month === 11) return "Dezember";
+}
+
+function getDaysInYear() {
+  if (isLeapYear === true) return 366;
+  if (isLeapYear === false) return 365;
+}
+
+function getWievielte() {
+  if (day < 8) return "erste";
+  if (day < 15) return "zweite";
+  if (day < 22) return "dritte";
+  if (day < 29) return "vierte";
+  if (day > 29) return "fünfte";
+}
 
 function getEasterSunday(year) {
   const a = year % 19;
@@ -164,8 +166,6 @@ function isHoliday() {
     return "ist kein gesetzlicher Feiertag";
   }
 }
-
-document.getElementById("holiday").textContent = isHoliday();
 
 /* 
 Ich will jetzt noch:
